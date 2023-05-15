6a592857ce23996092116cc852905bf50df93061
Wolf <wolf@paulus.com>
2023-05-15 16:54:51 -0700

    initial commit

--- ai_tutor.py
+++ ai_tutor.py
@@ -15,28 +15,52 @@ def print_wrapped(text: str) -> None:
         print(element)
 
 
-def ask(question: str, code: bool = False) -> str:
-    if code:
-        c = [
-            {"role": "user", "content": task},
-            {"role": "user", "content": "Provide feedback on the python implementation below."}
-        ]
-    else:
-        c = context
-    c.append({"role": "user", "content": question})
-    payload = dumps(c).encode(encoding="utf-8", errors="strict")
-    req = Request(server, method="POST")
-    req.add_header('User-Agent', 'Mozilla/5.0')
-    req.add_header('Content-Type', 'application/json; charset=utf-8')
-    req.add_header('Accept', 'application/json')
-    req.add_header('Content-Length', str(len(payload)))
-    with urlopen(req, timeout=100, data=payload) as resp:
-        status = resp.status
-        content = loads(resp.read().decode())
-        if status == 200:
-            if not code:
-                c.append({"role": "assistant", "content": content})
-        return content
+def ask(server: str, context: [], question: str = "Please enter your question: ", code: bool = False) -> str:
+    try:
+        payload = dumps(context).encode(encoding="utf-8", errors="strict")
+        req = Request(server, method="POST")
+        req.add_header('User-Agent', 'Mozilla/5.0')
+        req.add_header('Content-Type', 'application/json; charset=utf-8')
+        req.add_header('Accept', 'application/json')
+        req.add_header('Content-Length', str(len(payload)))
+        with urlopen(req, timeout=100, data=payload) as resp:
+            status = resp.status
+            content = loads(resp.read().decode())
+            if status == 200:
+                if not code:
+                    context.append({"role": "user", "content": question})
+                    context.append({"role": "assistant", "content": content})
+            return content
+    except ValueError as e:
+        print(e)
+        return "Hmm, did you initialize the tutor?"
+    except KeyboardInterrupt:
+        pass
+    except Exception as e:
+        return str(e.__cause__)
+
+
+def prompt(server, context: [], p: str = "Please enter your question: ") -> None:
+    print_wrapped(ask(server, context, question=input(p)))
+
+
+def validate(server: str, task: str) -> None:
+    context = [
+        {"role": "user", "content": task},
+        {"role": "user", "content": "Provide feedback on the python implementation below."}
+    ]
+    notebook_json_string = _message.blocking_request('get_ipynb', request='', timeout_sec=5)
+    code = "".join(notebook_json_string["ipynb"]["cells"][-2]["source"])
+    print(code)
+    print_wrapped(ask(server, context, code, code=True))
+
+
+def create_context(task: str, steps: str) -> []:
+    return [
+        {"role": "user", "content": task},
+        {"role": "user", "content": "How do I get started?"},
+        {"role": "assistant", "content": steps}
+    ]
 
 
 print("""Disclaimer: 
@@ -48,20 +72,3 @@ The AI Tutor is not a substitute for personalized instruction.
 Users are responsible for verifying code and applying concepts.
 The AI Tutor and its developers are not liable for errors or 
 consequences from tool usage.""")
-
-'''
-    setup the server, task, and context variables like so:`
-    server = "https://.../"
-    task = 'Create the first 100 prime numbers.'
-    context = [
-        {"role": "user", "content": task},
-        {"role": "user", "content": "How do I get started?"},
-        {"role": "assistant", "content": """1. Initialize an empty list to store the prime numbers.
-            2. Start with a number, let's say 2, and check if it is prime.
-            3. If the number is prime, add it to the list of primes.
-            4. Increment the number and repeat steps 2 and 3 until you have found n prime numbers."""}
-    ]
-'''
-server = ""
-task = ""
-context = []
--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
     name='ai_tutor',
-    version='0.1.0',
+    version='0.1.2',
     py_modules=['ai_tutor']
 )

f471bb09ac6f2b5c164ed0b7bd38e486d3b81478
Wolf <wolf@paulus.com>
2023-09-12 11:39:14 -0700

    move client files into src

--- mac/compress.sh
+++ mac/compress.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-# Compress the client folder into a zip file. 
-# This package will be downloaded by the notebook instances.
-cd ./client
+# Compress the ai_tutor.py and setup.py folder into a zip file. 
+# This package will be downloaded by the notebook instances. Like so:
+# ! pip install https://raw.githubusercontent.com/wolfpaulus/ai_tutor/main/package.zip > NUL
+cd ./src
 zip ../package.zip ./ai_tutor.py ./setup.py
 cd ..
